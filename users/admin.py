64aeebfd8e677ae0f20e7fe693a0640f55f12399
Svyatoslav <arend359@gmail.com>
2025-06-16 18:43:08 +0300

    Add custom user model with email as login

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """
+    Админка для кастомной модели пользователя.
+    """
+    model = CustomUser
+    list_display = ("email", "is_active", "is_staff")
+    ordering = ("email",)
+    search_fields = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
